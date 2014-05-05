8c962b0a34e2f74e15cf1f578d5db69b614c8b5b
pulse-matt <matt@pulsemicro.com>
2014-05-05 11:47:37 -0400

    Add replace text in template sample

---
+++ ReplaceTextInTemplate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pulse.VectorEngine;
+
+namespace ReplaceTextInTemplate
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			VectorDesign design = VectorDesign.Load(@"Template.PV", VectorDesignFileTypes.Pv);
+
+			// VectorDesign.ProcessTextObjects will iterate through the entire design to 
+			// find all Pulse.VectorEngine.TextObject objects and call the supplied delegate
+			// passing in the text object as the single parameter.
+			design.ProcessTextObjects((textObj) =>
+			{
+				if (textObj.Name == "Text1")
+				{
+					textObj.Text = "Replaced Text #1";
+				}
+				else if (textObj.Name == "Text2")
+				{
+					textObj.Text = "Replaced Text #2";
+				}
+			});
+
+			design.Render(@"ReplaceTextInTemplate.PNG", RenderFormats.Png, 96, design.ColourContext);
+		}
+	}
+}
